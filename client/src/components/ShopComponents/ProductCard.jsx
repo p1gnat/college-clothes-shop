644f82ce7a384f2fbadfb1f9a044a83b97ce1199
p1gnat <hellwowo1@gmail.com>
2025-04-22 00:01:07 +0500

    Shop/ just to fill in

--- client/src/components/ShopComponents/ProductCard.jsx
+++ client/src/components/ShopComponents/ProductCard.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./ProductCard.css";
+import CardDiscount from "../Body/CardDiscount";
 
 const ProductCard = () => {
-  return <div></div>;
+  return (
+    <div style={{ margin: "20px 0 40px 0" }}>
+      <CardDiscount
+        img="./main/cards/black-dress.png"
+        h3="Элегантное платье"
+        rating="3.5"
+        price="240"
+        oldPrice="260"
+        discount="20"
+      />
+    </div>
+  );
 };
 
 export default ProductCard;
